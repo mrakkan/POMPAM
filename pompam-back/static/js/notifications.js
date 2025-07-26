53f51918a3e181944ed2da5422960a4dbcfac46c
mrakkan <mansuperyoung4399@gmail.com>
2025-07-27 02:03:21 +0700

    noti-test-change-position

--- pompam-back/static/js/notifications.js
+++ pompam-back/static/js/notifications.js
@@ -158,7 +158,7 @@ class NotificationManager {
         // Create notification dropdown
         const notificationHTML = `
             <div id="notification-container" class="notification-container">
-                ${isIOS && isSafari && !isStandalone && !hasPushAPI ? '<div id="ios-banner" class="ios-banner">📱 สำหรับ iPhone: กดปุ่ม Share แล้วเลือก "Add to Home Screen" เพื่อรับการแจ้งเตือน</div>' : ''}
+                ${isIOS && isSafari && !isStandalone && !hasPushAPI ? '<div id="ios-banner" class="ios-banner">📱 สำหรับ iPhone: กดปุ่ม Share แล้วเลือก "Add to Home Screen" เพื่อรับการแจ้งเตือน<button class="ios-banner-close" onclick="document.getElementById(\'ios-banner\').remove()">×</button></div>' : ''}
                 <div id="notification-bell" class="notification-bell">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="white">
                         <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/>
